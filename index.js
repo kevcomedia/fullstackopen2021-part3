297a38959daef7e16f3333a269baefa5d6a6f888
Kev Comedia <kevcomedia@gmail.com>
2021-11-18 19:47:33 +0800

    Add route to show phonebook info

--- index.js
+++ index.js
@@ -28,6 +28,12 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+  response.send(`
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date().toString()}</p>`)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`listening to port ${PORT}`)
