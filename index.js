79504f073371391ba468b01b57f8c8c47932fc19
Kev Comedia <kevcomedia@gmail.com>
2021-11-23 18:22:29 +0800

    Get port from process.env

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ app.get('/info', (request, response) => {
     <p>${new Date().toString()}</p>`)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`listening to port ${PORT}`)
 })
