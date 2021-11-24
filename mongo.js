17e09f51c4586f7e4cdeef31909b9db21f21fcef
Kev Comedia <kevcomedia@gmail.com>
2021-11-24 17:13:02 +0800

    Store new entries to database

--- mongo.js
+++ mongo.js
@@ -11,4 +11,17 @@ const password = process.argv[2]
 const url = `mongodb+srv://fullstack:${password}@cluster0.ezgtm.mongodb.net/phonebook?retryWrites=true&w=majority`
 
 mongoose.connect(url)
-mongoose.connection.close()
+
+const personSchema = new mongoose.Schema({ name: String, number: String })
+const Person = mongoose.model('Person', personSchema)
+
+// for now assume that name and number are provided
+const person = new Person({
+  name: process.argv[3],
+  number: process.argv[4],
+})
+
+person.save().then(({ name, number }) => {
+  console.log(`added ${name} number ${number} to phonebook`)
+  mongoose.connection.close()
+})
